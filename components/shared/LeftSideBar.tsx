2dc8c69715093947567af1f865597940b0bc347d
Benjahh <lucasbenja09@hotmail.com>
2023-09-12 21:18:13 -0400

    left sidebar

--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -1,5 +1,12 @@
 function LeftSideBar() {
-    return <h1>LeftSideBar</h1>
+    return (
+        <section className="custom-scrollar leftsidenar"> 
+            <div className="flex flex-col flex-1 gap-6 px-6">
+
+            </div>
+
+        </section>
+    )
 }
 
 export default LeftSideBar
