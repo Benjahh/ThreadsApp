ba23b9e3dfc230cbdddede8b09c65c0e9cce2014
Benjahh <lucasbenja09@hotmail.com>
2023-09-17 20:33:20 -0400

    submit logic and validations

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -18,7 +18,8 @@ import {
   import * as z from "zod"
 import { Button } from "../ui/button"
 import Image from "next/image"
-import { ChangeEvent } from "react"
+import { ChangeEvent, useState } from "react"
+import { FILE } from "dns"
 
 interface Props {
     user: {
@@ -34,8 +35,10 @@ interface Props {
 }
 
 
-
 const AccountProfile = ({user, btnTitle } : Props) => {
+    const [files, setFiles] = useState<File[]>([])
+
+
     const form = useForm({
         resolver: zodResolver(UserValidation),
         defaultValues: {
@@ -46,8 +49,26 @@ const AccountProfile = ({user, btnTitle } : Props) => {
         }
     })
 
-    const handleImage = ( e: ChangeEvent, fieldChange: (value: string) => void ) => {
+    const handleImage = ( e: ChangeEvent<HTMLInputElement>, fieldChange: (value: string) => void ) => {
         e.preventDefault()
+
+        const fileReader = new FileReader()
+
+        if(e.target.files && e.target.files.length > 0) {
+            const file = e.target.files[0]
+
+            setFiles(Array.from(e.target.files))
+
+            if (!file.type.includes("image")) return
+
+            fileReader.onload = async (event) => {
+                const imageDataUrl = event.target?.result?.toString() || ""
+
+                fieldChange(imageDataUrl)
+            }
+
+            fileReader.readAsDataURL(file)
+        }
     }
 
     function onSubmit(values: z.infer<typeof UserValidation>) {
@@ -58,7 +79,7 @@ const AccountProfile = ({user, btnTitle } : Props) => {
         <Form {...form}>
             <form 
                 onSubmit={form.handleSubmit(onSubmit)}
-                className="flex flex-col justify-start">
+                className="flex flex-col justify-start gap-10">
                 <FormField
                     control={form.control}
                     name="profile_photo"
@@ -78,8 +99,8 @@ const AccountProfile = ({user, btnTitle } : Props) => {
                                 <Image
                                     src="/assets/profile.svg"
                                     alt="profile_photo"
-                                    width={96}
-                                    height={96}
+                                    width={24}
+                                    height={24}
                                     className="object-contain"
                                 />
                             )}
