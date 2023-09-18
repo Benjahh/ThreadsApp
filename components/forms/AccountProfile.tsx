fdbba16eff3a0c218491e9211efa557b55a17f17
Benjahh <lucasbenja09@hotmail.com>
2023-09-17 21:12:24 -0400

    mongo db connection and models

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,6 +20,9 @@ import { Button } from "../ui/button"
 import Image from "next/image"
 import { ChangeEvent, useState } from "react"
 import { FILE } from "dns"
+import { isBase64Image } from "@/lib/utils"
+import { useUploadThing } from "@/lib/uploadthing"
+import { start } from "repl"
 
 interface Props {
     user: {
@@ -37,6 +40,7 @@ interface Props {
 
 const AccountProfile = ({user, btnTitle } : Props) => {
     const [files, setFiles] = useState<File[]>([])
+    const { startUpload } = useUploadThing("media")
 
 
     const form = useForm({
@@ -71,8 +75,19 @@ const AccountProfile = ({user, btnTitle } : Props) => {
         }
     }
 
-    function onSubmit(values: z.infer<typeof UserValidation>) {
-        console.log(values)
+    const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+        const blob = values.profile_photo
+        const hasImageChanged = isBase64Image(blob)
+
+        if (hasImageChanged) {
+            const imgRes = await startUpload(files)
+
+            if(imgRes && imgRes[0].fileUrl) {
+                values.profile_photo = imgRes[0].fileUrl
+            }
+        }
+
+        
     }
 
     return (
