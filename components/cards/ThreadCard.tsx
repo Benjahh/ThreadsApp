7d6b4d346de386e6e8500070ca96e1e2ce7f9066
Benjahh <lucasbenja09@hotmail.com>
2023-09-19 22:23:18 -0400

    Threadcards image, comments and author formatting

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -63,7 +63,7 @@ const ThreadCard = ({
                     <p className="mt-2 text-small-regular text-light-2">{content}</p>
                     <div className="mt-5 flex flex-col gap-3">
                         <div className="flex gap-3.5">
-                            <Image src="/assets/heart-gray.svg" alt="heart" width={24} height={24} className="cursor-pointer object-contain">
+                            <Image src="/assets/heart-gray.svg" alt="heart" width={24} height={24} className="cursor-pointer object-contain"/>
                         </div>
                     </div>
                 </div>
