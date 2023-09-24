e03a89a07dc711f6ec7ac67960b2776a6589efb2
Benjahh <lucasbenja09@hotmail.com>
2023-09-24 11:58:51 -0400

    profileheader creation plus profile component

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -1,20 +1,19 @@
-import { currentUser } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
-import PostThread from "@/components/forms/PostThread";
-import { fetchUser } from "@/lib/actions/user.actions";
+import PostThread from '@/components/forms/PostThread';
+import { fetchUser } from '@/lib/actions/user.actions';
 
 async function Page() {
   const user = await currentUser();
   if (!user) return null;
 
- 
   const userInfo = await fetchUser(user.id);
-  if (!userInfo?.onboarded) redirect("/onboarding");
+  if (!userInfo?.onboarded) redirect('/onboarding');
 
   return (
     <>
-      <h1 className='head-text'>Create Thread</h1>
+      <h1 className="head-text">Create Thread</h1>
 
       <PostThread userId={userInfo._id} />
     </>
@@ -22,4 +21,3 @@ async function Page() {
 }
 
 export default Page;
-
